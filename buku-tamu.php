49ad955164745666953532d5b10f8b5e5d1f8c86
= <=>
2025-09-22 10:43:54 +0700

    menambahkan modal dan fitur tambah data tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -7,9 +7,34 @@ include_once('templates/header.php');
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
+                    <?php
+                    // jika ada tombol simpan
+                    if (isset($_POST['simpan'])) {
+                        if (tambah_tamu($_POST) > 0) {
+                    ?>
+                            <div class="alert alert-success" role="alert">
+                                Data berhasil disimpan!
+                            </div>
+                    <?php
+                        } else {
+                    ?>
+                            <div class="alert alert-danger" role="alert">
+                                Data gagal disimpan!
+                            </div>
+                    <?php
+                        }
+                    }
+                    ?>
+
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                            <button type="button" class="btn-primary btn-icon-split"
+                                data-toggle="modal" data-target="#tambahModal">
+                                <span class="icon text-white-50">
+                                    <i class="fas fa-plus"></i>
+                                </span>
+                                <span class="text">Data Tamu</span>
+                             </button>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
@@ -43,7 +68,7 @@ include_once('templates/header.php');
                                             <td><?php echo $tamu['alamat']; ?></td>
                                             <td><?php echo $tamu['no_hp']; ?></td>
                                             <td><?php echo $tamu['bertemu']; ?></td>
-                                            <td><?php echo $tamu['keperluan']; ?></td>
+                                            <td><?php echo $tamu['kepentingan']; ?></td>
                                             <td><button class="btn btn-success" type="button">Ubah</button>
                                                 <button class="btn btn-danger" type="button">Hapus</button>
                                             </td>
@@ -56,6 +81,70 @@ include_once('templates/header.php');
                     </div>
                 </div>
                 <!-- /.container-fluid -->
+<?php
+$query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar FROM buku_tamu");
+$data = mysqli_fetch_array($query);
+$kodeTamu = $data['kodeTerbesar'];
+
+$urutan = (int) substr($kodeTamu, 2, 3);
+$urutan++;
+$huruf = "zt";
+$kodeTamu = $huruf . sprintf("%03s", $urutan);
+?>
+<!-- Modal Tambah -->
+<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tambahModal">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+          <div class="form-group row">
+            <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+            <div class="col-sm-8">
+              <textarea class="form-control" id="alamat" name="alamat"></textarea>
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="no_hp" name="no_hp">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg. </label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="bertemu" name="bertemu">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="kepentingan" name="kepentingan">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+        <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+</div>
+
 <?php
 include_once('templates/footer.php')
 ?>
