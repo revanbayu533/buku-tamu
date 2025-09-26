ed7850d65df8b6547454f25c31193e0a62569d6e
= <=>
2025-09-26 07:15:37 +0700

    menambahkan fitur ganti password user

--- users.php
+++ users.php
@@ -23,6 +23,20 @@ include_once('templates/header.php');
           </div>
   <?php
       }
+  } else if (isset($_POST['ganti_password'])) {
+    if (ganti_password($_POST) > 0) {
+    ?>
+      <div class="alert alert-success" role="alert">
+        Password berhasil diubah!
+      </div>
+    <?php
+    } else {
+    ?>
+      <div class="alert alert-danger" role="alert">
+        Password gagal diubah!
+      </div>
+  <?php
+    }
   }
   ?>
 
@@ -59,7 +73,12 @@ include_once('templates/header.php');
                           <td><?php echo $no++; ?></td>
                           <td><?php echo $user['username']; ?></td>
                           <td><?php echo $user['user_role']; ?></td>
-                          <td><a class="btn btn-success" href="edit-user.php?id=<?= $user['id_user'] ?>">Ubah</a>
+                          <td>
+                             <button type="button" class="btn btn-info btn-icon-split" data-toggle="modal"
+                          data-target="#gantiPassword" data-id="<?= $user['id_user']?>">
+                            <span class="text">Ganti Password</span>
+                            </button>
+                            <a class="btn btn-success" href="edit-user.php?id=<?= $user['id_user'] ?>">Ubah</a>
                               <a onclick="confirm('Apakah anda yakin ingin menghapus data ini?')" class="btn btn-danger" 
                               href="hapus-user.php?id=<?= $user['id_user'] ?>">Hapus</a>
                           </td>
@@ -129,6 +148,36 @@ $kodeuser = $huruf . sprintf("%02s", $urutan);
     </div>
   </div>
 </div>
+
+<!-- Modal Ganti Password -->
+<div class="modal fade" id="gantiPassword" tabindex="-1" aria-labelledby="gantiPasswordLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="gantiPasswordLabel">Ganti Password</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <input type="hidden" name="id_user" id="id_user">
+          <div class="form-group row">
+            <label for="password" class="col-sm-3 col-form-label">Password Baru</label>
+            <div class="col-sm-7">
+              <input type="password" class="form-control" id="password" name="password">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+            <button type="submit" name="ganti_password" class="btn btn-primary">Simpan</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /.container-fluid -->
 
 <?php
 include_once('templates/footer.php')
